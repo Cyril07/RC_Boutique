c2291d5bcd6416787296990e1ee181d87c4cebed
Cyril07 <cy.lenglet@laposte.net>
2017-02-15 08:40:59 +0100

    Footer+page de présentation

--- src/FrontendBundle/Controller/DefaultController.php
+++ src/FrontendBundle/Controller/DefaultController.php
@@ -14,4 +14,36 @@ class DefaultController extends Controller
     {
         return $this->render('FrontendBundle::home.html.twig');
     }
+
+    /**
+	 * @Route("/qui-sommes-nous")
+	 */
+	public function aboutAction()
+	{
+		return $this->render('FrontendBundle:Layout:about_us.html.twig');
+	}
+
+	/**
+	 * @Route("/conditions-generales-de-vente")
+	 */
+	public function tofAction()
+	{
+		return $this->render('FrontendBundle:Layout:terms_of_sales.html.twig');
+	}
+
+	/**
+	 * @Route("/contact")
+	 */
+	public function contactAction ()
+	{
+		return $this->render('FrontendBundle:Layout:contact.html.twig');
+	}
+
+	/**
+	 * @Route("/livraison")
+	 */
+	public function shippingAction ()
+	{
+		return $this->render('FrontendBundle:Layout:shipping.html.twig');
+	}
 }
